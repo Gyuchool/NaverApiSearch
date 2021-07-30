36d0859e34b8d571a6b3c0ae91a78bbf3c5fe533
Gyuchool <balance0591@naver.com>
2021-07-30 11:30:20 +0900

    단위 테스트

--- src/main/java/com/springshop/naverapi/dto/ProductRequestDto.java
+++ src/main/java/com/springshop/naverapi/dto/ProductRequestDto.java
@@ -1,8 +1,10 @@
 package com.springshop.naverapi.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ProductRequestDto {
     private String title;
     private String link;
