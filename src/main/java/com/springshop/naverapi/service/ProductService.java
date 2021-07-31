aa134c8a9305b98d052c30727cf6d9c3b22b184a
Gyuchool <balance0591@naver.com>
2021-07-31 10:49:14 +0900

    통합 테스트 작성

--- src/main/java/com/springshop/naverapi/service/ProductService.java
+++ src/main/java/com/springshop/naverapi/service/ProductService.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private static final int MIN_PRICE = 100;
 
     // 회원 ID 로 등록된 모든 상품 조회
     public List<Product> getProducts(Long userId) {
@@ -48,6 +49,22 @@ public class ProductService {
         product.updateByItemDto(itemDto);
         return id;
 
+    }
+
+    @Transactional // 메소드 동작이 SQL 쿼리문임을 선언합니다.
+    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
+        );
+
+        // 변경될 관심 가격이 유효한지 확인합니다.
+        int myPrice = requestDto.getMyprice();
+        if (myPrice < MIN_PRICE) {
+            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_PRICE + " 원 이상으로 설정해 주세요.");
+        }
+
+        product.updateMyPrice(myPrice);
+        return product;
     }
 
     // 모든 상품 조회 (관리자용)
