9a7e0fa02e4553fc72632884000faf8be2c70e71
Gyuchool <balance0591@naver.com>
2021-08-12 13:49:47 +0900

    gradlew 수정

--- src/main/java/com/springshop/naverapi/NaverapiApplication.java
+++ src/main/java/com/springshop/naverapi/NaverapiApplication.java
@@ -3,6 +3,7 @@ package com.springshop.naverapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
