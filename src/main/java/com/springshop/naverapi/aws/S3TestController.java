36379657f640fa7811f71c416b8d2f85470467bd
Gyuchool <balance0591@naver.com>
2021-08-13 17:21:22 +0900

    deploy수정

--- src/main/java/com/springshop/naverapi/aws/S3TestController.java
+++ src/main/java/com/springshop/naverapi/aws/S3TestController.java
@@ -1,7 +1,6 @@
 package com.springshop.naverapi.aws;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
