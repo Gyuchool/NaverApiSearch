c877f9199764835fcab641c18e59a29f3ed1f074
Gyuchool <balance0591@naver.com>
2021-08-16 15:05:36 +0900

    yml정리

--- src/main/java/com/springshop/naverapi/NaverapiApplication.java
+++ src/main/java/com/springshop/naverapi/NaverapiApplication.java
@@ -1,11 +1,19 @@
 package com.springshop.naverapi;
 
+import com.springshop.naverapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableScheduling
 @EnableJpaAuditing
@@ -13,14 +21,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class NaverapiApplication {
 
 
-        public static final String APPLICATION_LOCATIONS = "spring.config.location="
-                + "classpath:application.yml,"
-                + "classpath:application-aws.yml";
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:application-aws.yml";
     public static void main(String[] args) {
 
-            new SpringApplicationBuilder(NaverapiApplication.class)
-                    .properties(APPLICATION_LOCATIONS)
-                    .run(args);
+        new SpringApplicationBuilder(NaverapiApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
 //         SpringApplication.run(NaverapiApplication.class, args);
     }
 
