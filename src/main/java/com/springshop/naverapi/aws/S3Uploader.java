aceea9921d7e36cba86b2391ea16bbc9fd4342f9
Gyuchool <balance0591@naver.com>
2021-09-09 01:40:25 +0900

    profile 설정

--- src/main/java/com/springshop/naverapi/aws/S3Uploader.java
+++ src/main/java/com/springshop/naverapi/aws/S3Uploader.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,6 +16,7 @@ import java.io.IOException;
 import java.util.Optional;
 import java.util.UUID;
 
+@Profile("stratospheric")
 @Slf4j
 @Component
 @RequiredArgsConstructor
