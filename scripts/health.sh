5c7f6aa604c8d4742eb1b9e680790b9581c11f00
Gyuchool <balance0591@naver.com>
2021-09-09 22:17:38 +0900

    test

--- scripts/health.sh
+++ scripts/health.sh
@@ -16,7 +16,7 @@ for RETRY_COUNT in {1..10}  # for문 10번 돌기
 do
   RESPONSE=$(curl -s http://13.124.116.76:${IDLE_PORT}/profile)   # 현재 문제 없이 잘 실행되고 있는 요청을 보내봅니다.
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)     # 해당 결과의 줄 수를 숫자로 리턴합니다.
-  echo "> new Port: ${RESPONSE}"
+  echo "> new Port: http://13.124.116.76:${IDLE_PORT}/profile"
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
       echo "> Health check 성공"
