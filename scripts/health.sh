b15955e7fef8cbe850969ac7027dee3b33df1c39
Gyuchool <balance0591@naver.com>
2021-09-10 16:51:56 +0900

    healt 수정

--- scripts/health.sh
+++ scripts/health.sh
@@ -14,10 +14,10 @@ sleep 10
 
 for RETRY_COUNT in {1..10}  # for문 10번 돌기
 do
-  RESPONSE=$(curl -s http://13.124.116.76:$IDLE_PORT/profile)   # 현재 문제 없이 잘 실행되고 있는 요청을 보내봅니다.
-  UP_COUNT=$(echo $RESPONSE | grep 'real' | wc -l)     # 해당 결과의 줄 수를 숫자로 리턴합니다.
+  RESPONSE=$(curl -s http://13.124.116.76:${IDLE_PORT}/profile)   # 현재 문제 없이 잘 실행되고 있는 요청을 보내봅니다.
+  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)     # 해당 결과의 줄 수를 숫자로 리턴합니다.
 
-  if [ $UP_COUNT -ge 1 ]
+  if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
       echo "> Health check 성공"
       switch_proxy   # switch.sh 실행
