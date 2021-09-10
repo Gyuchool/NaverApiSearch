27bb64f7a67a24d7593473883e7d32f5e939a6b6
Gyuchool <balance0591@naver.com>
2021-09-10 17:18:36 +0900

    start 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,9 +7,9 @@ REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=naver-api-giron
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/zip/build/libs *.jar $REPOSITORY/"
+echo "> cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip/build/libs *.jar $REPOSITORY/
+cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
@@ -25,7 +25,7 @@ IDLE_PROFILE=$(find_idle_profile)
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application.yml \
+    -Dspring.config.location=classpath:/application-aws.yml,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application.yml \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
