cad81ee25ee182daec2e42578dd6913f0472d83c
Gyuchool <balance0591@naver.com>
2021-09-15 15:59:58 +0900

    test

--- src/main/java/com/springshop/naverapi/utils/Scheduler.java
+++ src/main/java/com/springshop/naverapi/utils/Scheduler.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-@RequiredArgsConstructor // final 멤버 변수를 자동으로 생성합니다.
+@RequiredArgsConstructor
 @Component // 스프링이 필요 시 자동으로 생성하는 클래스 목록에 추가합니다.
 public class Scheduler {
 
@@ -35,7 +35,7 @@ public class Scheduler {
             String resultString = naverShopSearch.search(title);
             // i 번째 관심 상품의 검색 결과 목록 중에서 첫 번째 결과를 꺼냅니다.
             List<ItemDto> itemDtoList = naverShopSearch.fromJSONtoItems(resultString);
-            ItemDto itemDto = itemDtoList.get(0);//???????????????????? 내가 저장한게 0번째인가??
+            ItemDto itemDto = itemDtoList.get(0);
             // i 번째 관심 상품 정보를 업데이트합니다.
             Long id = p.getId();
             productService.updateBySearch(id, itemDto);
