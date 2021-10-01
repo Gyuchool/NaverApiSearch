55edfbed0d68327327b3e688e7dc3352c9499a75
Gyuchool <balance0591@naver.com>
2021-10-01 09:20:47 +0900

    test: assertAll 적용(Junit5)

--- src/test/java/com/springshop/naverapi/JunitTest.java
+++ src/test/java/com/springshop/naverapi/JunitTest.java
@@ -4,26 +4,20 @@ import com.springshop.naverapi.models.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import java.time.Duration;
-
-import static java.time.Duration.ofMillis;
-import static java.time.Duration.ofMinutes;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class JunitTest {
-    @RepeatedTest(10)
-    @DisplayName("반복 테스트")
-    void repeatedTest() {
 
+    @Test
+    public void createProduct(){
+        Product product = new Product();
+        assertAll(
+                () -> assertNotNull(product),
+                () -> assertEquals(product.getMyprice(), 5000),
+                () -> assertTrue(product.getLprice() < 1000, "1000원보다는 비싸야 한다.")
+        );
     }
 
-    @RepeatedTest(value = 10, name = "{displayName} 중 {currentRepetition}")
-    @DisplayName("반복 테스트2")
-    void repeatedTest2(){
-
-    }
 }
